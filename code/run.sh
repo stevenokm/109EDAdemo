b5c08e1edbcb59d0e634ecfbc923bf5e29454be6
Yi-Wen Hung <stevenokmijn@gmail.com>
2020-07-13 18:09:47 +0800

    update run script 	modified:   run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,2 +1,2 @@
 sudo docker run --gpus 1 --rm -it -v ${PWD}:/home/workspace -v ${PWD}/../../SEM:/home/workspace/SEM -w /home/workspace pytorch_env/pytorch:1.4_py36_cu100 \
-    python3 main.py --batch-size 4 --duplicate 20 --COT --sess COT_session
+    python3 main.py --batch-size 8 --duplicate 20 --COT --sess COT_session
