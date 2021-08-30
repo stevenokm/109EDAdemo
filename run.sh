c53ed84a055b6a6e32a1eca2dcf3486bcb723bef
Yi-Wen Hung <stevenokmijn@gmail.com>
2021-08-30 16:44:39 +0800

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -19,17 +19,18 @@
 #  --sess baseline \
 #  $@
 
-#export CUDA_VISIBLE_DEVICES=0,1
-#python3.6 main.py \
-#  --batch-size 32 \
-#  --workers 16 \
-#  --lr 0.1 \
-#  --epochs 1 \
-#  --seed 22222 \
-#  --sess brevitas \
-#  $@
+export CUDA_VISIBLE_DEVICES=0,1
+python3.6 main.py \
+  --batch-size 32 \
+  --workers 16 \
+  --lr 0.1 \
+  --epochs 100 \
+  --seed 22222 \
+  --sess brevitas \
+  --export_finn \
+  $@
 
-#export CUDA_VISIBLE_DEVICES=1,2
+#export CUDA_VISIBLE_DEVICES=0,1,2,3
 #python3.6 -O main.py \
 #  --batch-size 32 \
 #  --workers 16 \
@@ -37,6 +38,17 @@
 #  --epochs 100 \
 #  --seed 11111 \
 #  --sess brevitas \
+#  --export_finn \
+#  $@
+
+#export CUDA_VISIBLE_DEVICES=0,1,2,3
+#python3.6 -O main.py \
+#  --batch-size 32 \
+#  --workers 16 \
+#  --lr 0.1 \
+#  --epochs 100 \
+#  --seed 11111 \
+#  --sess brevitas_wsconv \
 #  --export_finn \
 #  $@
 
@@ -48,6 +60,18 @@
 #  --epochs 5 \
 #  --seed 11111 \
 #  --sess M5 \
+#  --export_finn \
+#  --optimizer Adam \
+#  $@
+
+#export CUDA_VISIBLE_DEVICES=0
+#python3.6 -O main.py \
+#  --batch-size 128 \
+#  --workers 16 \
+#  --lr 0.01 \
+#  --epochs 5 \
+#  --seed 11111 \
+#  --sess M5_wsconv \
 #  --export_finn \
 #  --optimizer Adam \
 #  $@
@@ -64,22 +88,22 @@
 #  --optimizer Adam \
 #  $@
 
-export CUDA_VISIBLE_DEVICES=2
-python3.6 -O main.py \
-  --batch-size 128 \
-  --workers 16 \
-  --lr 0.1 \
-  --epochs 30 \
-  --seed 11111 \
-  --sess end2end \
-  --export_finn \
-  --optimizer SGD \
-  $@
-
+#export CUDA_VISIBLE_DEVICES=2
 #python3.6 -O main.py \
-#  --batch-size 32 \
+#  --batch-size 128 \
 #  --workers 16 \
+#  --lr 0.1 \
+#  --epochs 30 \
 #  --seed 11111 \
-#  --sess brevitas \
-#  --resume \
+#  --sess end2end \
+#  --export_finn \
+#  --optimizer SGD \
 #  $@
+
+python3.6 -O main.py \
+  --batch-size 32 \
+  --workers 16 \
+  --seed 11111 \
+  --sess brevitas \
+  --resume \
+  $@
